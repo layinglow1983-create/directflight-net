9e9f1774584cc98b5bd1c71344de2976ba1e84be
layinglow1983-create <layinglow1983@gmail.com>
2026-05-26 08:44:46 -0600

    Update site URL in astro.config.mjs

--- directflight-astro/astro.config.mjs
+++ directflight-astro/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+
   site: 'https://www.directflight.net',
-  integrations: [sitemap()],
+
   output: 'static',
+
 });
